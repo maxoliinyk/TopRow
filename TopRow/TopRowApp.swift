6f79f351198cb76e3666645f7f907f9ab5770c31
maxoliinyk <i4m.oliinyk@gmail.com>
2026-08-13 19:22:05 +0200

    Initial Commit

---
+++ TopRow/TopRowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopRowApp.swift
+//  TopRow
+//
+//  Created by Max Oliinyk on 13.08.26.
+//
+
+import SwiftUI
+
+@main
+struct TopRowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
